e837f4a20f09a84e50d210fbdec26c3fe260cc1a
Bobby Willmes <bobbywillmes@gmail.com>
2021-02-19 09:44:49 -0800

    added js to add new item

--- js/application.js
+++ js/application.js
@@ -1,23 +1,66 @@
 $(document).ready(function() {
   console.log(`$(document).ready`)
 
-  $('table input').on('change', function() {
-    let qty = $(this).val()
-    console.log(qty)
-    let price = $(this).parent().prev().text()
-    price = parseFloat(price)
-    console.log(price)
-    let itemTotal = qty * price
-    itemTotal = itemTotal * 100
-    itemTotal = parseInt(itemTotal)
-    itemTotal = itemTotal / 100
-    console.log(itemTotal)
-    let last = $(this).closest('tr').children('.subtotal').text(itemTotal)
-    console.log(last)
-    totalPrice()
-  })
+  $('tbody').on('change', 'input', calculatePrice)
+
+  $('tfoot').on('change', enableAdd)
+
+  $('#add').on('click', addItem)
 
 })
+
+function calculatePrice() {
+  let qty = $(this).val()
+  console.log(qty)
+  let price = $(this).parent().prev().text()
+  price = parseFloat(price)
+  console.log(price)
+  let itemTotal = qty * price
+  itemTotal = itemTotal * 100
+  itemTotal = parseInt(itemTotal)
+  itemTotal = itemTotal / 100
+  console.log(itemTotal)
+  let last = $(this).closest('tr').children('.subtotal').text(itemTotal)
+  console.log(last)
+  totalPrice()
+}
+
+function enableAdd() {
+  console.log(`enableAdd()`)
+  let tfoot = $(this)
+  let item = tfoot.find('.item').val()
+  let price = tfoot.find('.price').val()
+  console.log(`${item}  ${price}`)
+  // enable Add button once item & price are filled out
+  if(item !== '' && price !== '') {
+    console.log(`item can be added`)
+    tfoot.find('#add').prop('disabled', false)
+  } else {
+    console.log(`cant add item yet`)
+    tfoot.find('#add').prop('disabled', true)
+  }
+}
+
+function addItem() {
+  console.log(`add clicked`)
+  let row = $(this).parent().parent()
+  let item = row.find('.item').val()
+  let price = row.find('.price').val()
+  console.log(`${item}  ${price}`)
+  // create new row, then add to table
+  let newRow = `
+    <tr>
+      <td>${item}</td>
+      <td>${price}</td>
+      <td><input type="number" min="0"/></td>
+      <td class="subtotal">-</td>
+    </tr>`
+  $('table tbody').append(newRow)
+  // clear input elements
+  row.find('.item').val('')
+  row.find('.price').val('')
+  $('#add').prop('disabled', true)
+}
 
 function totalPrice() {
   console.log(`totalPrice`)
